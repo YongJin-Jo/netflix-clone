36c31b8691a44a2dca647bb4b43b6a5077fe349f
artinc5233 <artinc5233@gmail.com>
2022-02-12 17:50:22 +0900

    Movie interface 추가 정의

--- src/type/movieDefind/index.ts
+++ src/type/movieDefind/index.ts
@@ -1,9 +1,18 @@
 interface Movies {
+  adult: boolean;
   backdrop_path: string;
+  genre_ids: number[];
   id: number;
+  original_language: string;
+  original_title: string;
   overview: string;
-  title: string;
+  popularity: number;
   poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
 }
 
 interface IGetMoviesResult {
